c412124877ac78eb527526a7d282df5b5953c8dd
ramiro-solano <rramiro.solano@gmail.com>
2025-03-23 01:08:31 -0300

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { every } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,64 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'maia-mis-15';
   isPause = true
-  private audio: HTMLAudioElement;
+  endDate: Date =  new Date(2025, 3, 12, 21, 30, 0);
+	reaminingDays!: number;
+	remainingHours!: number;
+	remainingMinutes!: number;
+	remainingSeconds!: number;
+	intervalId: any;
+  	private audio: HTMLAudioElement;
 
 	constructor() {
 		this.audio = new Audio('assets/song/song.mp3');
 	}
 
+	ngOnInit(): void {
+		this.updateCountdown();
+		this.intervalId = setInterval(() => this.updateCountdown(), 1000);
+	}
+
+	ngOnDestroy(): void {
+		clearInterval(this.intervalId);
+	}
+
+	updateCountdown(): void {
+		const now = Date.now();
+		const difference = this.endDate.getTime() - now;
+
+		if (difference <= 0) {
+			// La cuenta regresiva ha terminado
+			this.remainingHours = 0;
+			this.remainingMinutes = 0;
+			this.remainingSeconds = 0;
+			clearInterval(this.intervalId);
+			return;
+		}
+
+		this.reaminingDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+		if (this.reaminingDays > 0) {
+			this.remainingHours = Math.floor(difference / (1000 * 60 * 60)) - (this.reaminingDays * 24);
+		}
+		else {
+			this.remainingHours = Math.floor(difference / (1000 * 60 * 60));
+		}
+		this.remainingMinutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+		this.remainingSeconds = Math.floor((difference % (1000 * 60)) / 1000);
+	}
+
+
+
+	isModalOpen = true; // Se abre al cargar la página
+
+  confirmEntry() {
+    this.isModalOpen = false;
+    console.log("Usuario entró a la invitación");
+    // Aquí puedes agregar una redirección o lógica adicional
+  }
+
   playMusic() {
+		this.confirmEntry();
+
 		if (this.isPause) {
 			this.audio.play();
 		} else {
@@ -27,7 +79,7 @@ export class AppComponent {
 
   copy(event: MouseEvent) {
 		// Obtener el elemento padre del ícono de copia (el elemento <p>)
-		const pElement = (event.target as HTMLElement).closest('p');
+		const pElement = (event.target as HTMLElement).closest('span');
 		
 		if (pElement) {
 			// Obtener el texto del elemento <p>
@@ -42,7 +94,12 @@ export class AppComponent {
 				const successful = document.execCommand('copy');
 				if (successful) {
 					pElement.classList.remove('bg-slate-200');
-					pElement.classList.add('bg-green-200');
+					pElement.classList.add('bg-purple-500/50');
+
+					setTimeout(() => {
+						pElement.classList.remove('bg-purple-500/50');
+						pElement.classList.add('bg-slate-200');
+					}, 2000);
 				} else {
 					console.error('Error al copiar el texto');
 				}
@@ -57,9 +114,12 @@ export class AppComponent {
 		}
 	}
 
-	openwhatsapp() {
-		window.open('https://wa.me/+5493815585823?text=Hola%20quiero%20confirmar%20mi%20asistencia%20al%2015%20de%20Maia.');
+	openwhatsapp(numero: number) {
+		const mensaje = encodeURIComponent("Hola, quiero confirmar mi asistencia al 15 de Maia.");
+		const url = `https://wa.me/+549${numero}?text=${mensaje}`;
+		window.open(url, "_blank");
 	}
+	
 
 	salonMap() {
 		window.open('https://maps.app.goo.gl/oDMJWASmpfomwMxv8');
